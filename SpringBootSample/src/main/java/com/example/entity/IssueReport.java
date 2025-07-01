712cfae601d9c4f8249d65a8fcfba7e68a9e7f40
dinesh-veer <dveer123@hotmail.com>
2025-07-01 21:27:58 +0530

    Updated library to jakarta

--- SpringBootSample/src/main/java/com/example/entity/IssueReport.java
+++ SpringBootSample/src/main/java/com/example/entity/IssueReport.java
@@ -1,17 +1,13 @@
 package com.example.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="issues")
 public class IssueReport {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy= GenerationType.AUTO)
 	private long id;
 	private String name;
 	private String email;
