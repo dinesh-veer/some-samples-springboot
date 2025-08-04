e09d2b08c575edcf26577d0ac88d4f62dfc6775d
dinesh-veer <dveer123@hotmail.com>
2025-08-04 22:07:40 +0530

    updated code

--- AuthTokenGeneration/src/main/java/com/spring/authtokengeneration/model/AuthResponse.java
+++ AuthTokenGeneration/src/main/java/com/spring/authtokengeneration/model/AuthResponse.java
@@ -1,13 +1,13 @@
 package com.spring.authtokengeneration.model;
 
+import lombok.Getter;
+
+@Getter
 public class AuthResponse {
-    private String token;
+    private final String token;
 
     public AuthResponse(String token) {
         this.token = token;
     }
 
-    public String getToken() {
-        return token;
-    }
 }
